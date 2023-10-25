ac1acf792c0268dc11c40ad0fa3d647b51e53857
vladimir <vovchikbura@mail.ru>
2023-10-25 17:57:46 +0300

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Script from 'next/script';
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+    return (
+        <Layout>
+        </Layout>
+    );
+}
